9bfa6f4e4453f87d318c43de23d1bdc92827bc69
seekment <ylyang@email.swu.edu.cn>
2022-07-10 18:04:58 +0800

    多个线程同时操作一个对象 买火车票的例子 ,带来了并发问题，数据不一致

--- src/com.yang/demo02/TestThread2.java
+++ src/com.yang/demo02/TestThread2.java
@@ -1,5 +1,7 @@
 package demo02;
 
+import java.io.IOException;
+
 /**
  * @ClassName TestThread2
  * @Author yang
@@ -9,7 +11,31 @@ package demo02;
  **/
 
 // 多个线程同时操作一个对象
-    // 买火车票的例子
-public class TestThread2 {
+    // 买火车票的例子 ,带来了并发问题，数据不一致
+public class TestThread2 implements Runnable {
+    // 票数
+    private int ticketNums = 10;
 
+    @Override
+    public void run() {
+        while (true) {
+            if (ticketNums <= 0) break;
+            try {
+                Thread.sleep(200);
+                System.out.println(Thread.currentThread().getName() + "-->拿到了第" + ticketNums-- + "票");
+            }catch ( InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        TestThread2 ticket =    new TestThread2();
+        new Thread(ticket,"小明").start();
+        new Thread(ticket,"小红").start();
+        new Thread(ticket,"黄牛党").start();
+        System.out.println("main");
+    }
 }
+
